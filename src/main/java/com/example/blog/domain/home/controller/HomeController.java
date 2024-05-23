59197b7cada8a358693bb833b2246fe1cfe097e1
jiyoung-OyO <kimwldud1234@gmail.com>
2024-05-23 17:58:08 +0900

    로그인 기능 구현을 위한 세팅

--- src/main/java/com/example/blog/domain/home/controller/HomeController.java
+++ src/main/java/com/example/blog/domain/home/controller/HomeController.java
@@ -7,8 +7,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class HomeController {
     @GetMapping("/")
-    @ResponseBody
     public String home() {
-        return "나는 밥오 에요 :)";
+        return "home/main";
     }
 }
