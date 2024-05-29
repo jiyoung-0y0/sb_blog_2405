07643cc33249cdf00bb3028fa21a9d7270ccdc6c
jiyoung-OyO <kimwldud1234@gmail.com>
2024-05-29 14:35:26 +0900

    게시글 등록 작업

--- src/main/java/com/example/blog/BlogApplication.java
+++ src/main/java/com/example/blog/BlogApplication.java
@@ -1,9 +1,12 @@
 package com.example.blog;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class BlogApplication {
 
 	public static void main(String[] args) {
